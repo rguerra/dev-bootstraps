21804a7f5eda383defba578a49d5f287980bc329
Raul <>
2021-04-11 20:55:35 +0000

    Adding role to clone repos

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #ansible-playbook -vvv site.yml -i hosts.yml
-ansible-playbook -i hosts playbook.yml
+ansible-playbook -i hosts playbook.yml --vault-password-file ~/.vault_pass.txt
