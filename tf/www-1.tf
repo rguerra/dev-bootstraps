368a2e20dcf6d6880604b987047b44410cee4665
Raul <>
2021-04-19 02:13:01 +0000

    installing jupyterlab at least

--- tf/www-1.tf
+++ tf/www-1.tf
@@ -7,7 +7,7 @@ resource "digitalocean_ssh_key" "default" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 resource "digitalocean_droplet" "www-1" {
-    image = "ubuntu-18-04-x64"
+    image = "ubuntu-20-04-x64"
     name = "www-1"
     region = "nyc3"
     size = "s-4vcpu-8gb"
