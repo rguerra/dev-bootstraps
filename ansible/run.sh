7b693e7a1e6cfc510fc12859e32402710db4ab12
Raul <>
2021-04-23 20:30:51 +0000

    Docker image working

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-
+if [ $# -eq 0 ]
+  then
+    export VERBOSE=""
+  else
+    export VERBOSE=$1
+fi
 #ansible-playbook -vvv site.yml -i hosts.yml
-ansible-playbook -vvvv -i hosts playbook.yml --vault-password-file ~/.vault_pass.txt
+ansible-playbook $VERBOSE -i hosts playbook.yml --vault-password-file ~/.vault_pass.txt
