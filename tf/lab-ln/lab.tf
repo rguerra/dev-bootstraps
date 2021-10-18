263d5970b85674fd0fddb77fc30751d33708a0d6
rguerr208 <raul_guerraparedes@comcast.com>
2021-10-18 14:30:38 -0400

    Opening ssh connection

--- tf/lab-ln/lab.tf
+++ tf/lab-ln/lab.tf
@@ -10,6 +10,18 @@ resource "linode_instance" "lab" {
         authorized_keys = [ 
 	   chomp(file(var.pub_key))
 	]
+        connection {
+                host = self.ip_address
+                user = "root"
+                type = "ssh"
+                private_key = file(var.pvt_key)
+                timeout = "2m"
+        }
+        provisioner "remote-exec" {
+        inline = [
+                "export PATH=$PATH:/usr/bin",
+        ]
+  }
 
 }
 
