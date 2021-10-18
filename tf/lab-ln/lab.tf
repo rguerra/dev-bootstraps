8fe6c6f6c49f132f5f0a3526d65f7a5110209140
rguerr208 <raul_guerraparedes@comcast.com>
2021-10-18 15:02:17 -0400

    Adding ssh key

--- tf/lab-ln/lab.tf
+++ tf/lab-ln/lab.tf
@@ -1,5 +1,9 @@
 
 
+resource "linode_sshkey" "ln_ssh_key" {
+  label       = "ln_ssh_key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
 
 resource "linode_instance" "lab" {
         image = "linode/ubuntu20.04"
