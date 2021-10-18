6337dbd26ee81e57da79807e1ef516f1c8b78cde
Raul Guerra <rguerrp@localhost>
2021-10-18 18:09:01 +0000

    Customizing

--- tf/lab-ln/outputs.tf
+++ tf/lab-ln/outputs.tf
@@ -1,8 +1,8 @@
 resource "local_file" "AnsibleInventory" {
 content = templatefile("inventory.tmpl",
         {
-            lab-ip = linode_instance.lab.ipv4,
-            lab-id = linode_instance.lab.id,
+            lab-ip = linode_instance.lab.ip_address,
+            lab-id = linode_instance.lab.label,
         }
     )
  filename = "../../ansible/lab_hosts"
